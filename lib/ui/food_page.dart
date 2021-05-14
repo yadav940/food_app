483921186030ed25058780350b085cefccfc2e89
vinod yadav <vinod@modistabox.com>
2021-05-14 21:25:08 +0530

    Initial commit

---
+++ lib/ui/food_page.dart
@@ -0,0 +1,21 @@
+//create by vinod.yadav940@gmail.com at 14/5/21
+import 'package:flutter/material.dart';
+
+class FoodPage extends StatelessWidget {
+  bool value = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Details'),
+        ),
+        body: ListView(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
